1a558112e0c520550e70830f164b17960e7375cd
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-06-25 09:34:58 +0100

    Added EMACs customisation files for using REPL  and other customisations

---
+++ .emacs.d/elpa/pycomplete-1.0/pycomplete-autoloads.el
@@ -0,0 +1,15 @@
+;;; pycomplete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("pycomplete.el") (22862 38966 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pycomplete-autoloads.el ends here
---
+++ .emacs.d/elpa/pycomplete-1.0/pycomplete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pycomplete" "1.0" "Complete symbols at point using Pymacs." 'nil)
